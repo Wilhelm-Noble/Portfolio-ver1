76126dcaeb012ccd876fc94a64feb7b1b7b01626
Wilhelm-Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2022-03-20 17:56:27 +0800

    Add files via upload

---
+++ resources/script.js
@@ -0,0 +1,17 @@
+new fullpage('#fullpage', {
+  
+    sectionsColor: ['#1F2833', '#19c3d4', '#1F2833', '#19c3d4', '#1F2833'],
+    autoScrolling: false,
+    navigation: true,
+    anchors: ['1stPage', '2ndPage', '3rdPage', '4thpage', '5thpage'],
+            menu: '#menu',
+            css3: true,
+            scrollingSpeed: 1000
+    
+    
+    
+  });
+
+
+
+
